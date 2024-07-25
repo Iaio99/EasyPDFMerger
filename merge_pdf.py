a4b7e97119cf183867793cac8804d7966feb3047
Iaio99 <giuliano.vallone@ikmail.com>
2024-07-25 10:49:39 +0200

    Ora è possibile scegliere se eliminare i file originali oppure no

--- merge_pdf.py
+++ merge_pdf.py
@@ -12,6 +12,14 @@ parser.add_argument('-b', '--no-bookmarks', dest='bookmarks', action='store_fals
 parser.add_argument('-i', '--import-bookmarks', action='store_true')
 
 
+def yes_or_no(question: str):
+    answer = input(f"{question} [y/N]: ")
+    if answer.lower() == "yes" or answer.lower() == "y":
+        return True
+    else:
+        return False
+
+
 def get_bookmarks(pdf_file: PdfReader):
     bookmarks = {}
     
@@ -49,6 +57,10 @@ def merge_pdfs_with_bookmark(pdf_files: list, output_file: str, bookmarks = True
     with open(output_file, 'wb') as out_pdf:
         print("Writing output")
         pdf_writer.write(out_pdf)
+
+    if yes_or_no("Do you want to remove the original files?"):
+        for p in pdf_files:
+            os.remove(p)
 
 
 if __name__ == "__main__":
